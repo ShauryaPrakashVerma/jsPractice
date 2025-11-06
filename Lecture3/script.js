d694f25cbb4dffd54c6901b42a22361b5e7d4b1b
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-06 22:00:58 +0530

    Complete lecture 3.

---
+++ Lecture3/script.js
@@ -0,0 +1,55 @@
+// ARITHMETIC OPERATORS
+// + --> addition
+// - --> subtraction
+// * --> multiplication
+// / --> divide (quotient)
+// % --> modulus (remainder)
+// ** --> exponentiation (Power)
+
+
+// ASSIGNMENT OPERATOR
+// =  --> assignment operator(equals)
+// +=
+// -=
+// *=
+// /=
+// *=
+
+
+// COMPARISON OPERATORS
+// ==  --> equality check operator. Does not check data type
+// ===  --> strict equality check operator. Checks data type
+// !=  --> not equal to  (opposite of "==")
+// !==  --> not strictly equal to (opposite of "===")
+// <=  --> less than equal to
+// >=  --> greater than or equal to
+
+
+// LOGICAL OPERATORS
+// && --> Logical AND (Both sides should be true for output to be true)
+// || --> Logical OR (One side should be true for output to be true)
+// ! --> Logical NOT (Reverse)
+
+
+// UNARY OPERATOR (works on a single operand)
+// + --> addition
+    // Trick: to convert a string into a number put "+" before it
+        // +"12" --> 12
+        // +"Shaurya" --> NaN 
+// - --> subtraction
+// ! --> not
+// typeof --> to know the data type of any value
+// ++ --> pre-increment or post-increment
+// -- --> pre-decrement or post-decrement
+
+
+// Ternary Operator
+// ?: --> SYNTAX: <condition> ? <expression if true> : <expression if false>
+
+
+// typeof
+// basically works with primitive data types
+
+
+// instanceof
+// works only with reference data types
