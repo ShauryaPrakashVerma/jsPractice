ef270ee0c95ebd20501589f410758590ea27c5d4
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-07 23:48:19 +0530

    Complete Lecture 3 on Loops.

---
+++ Lecture4/script.js
@@ -0,0 +1,18 @@
+//LOOP
+
+// for loop
+// kaha se jaana hai --> kahan tak jaana hai --> kaise jaana hai
+
+
+// while loop
+// kahan se jaana hai --> kab rukna hai --> kaise jaana hai
+    // start
+    // while (end) {
+
+    //     change
+    // }
+
+// foreach loop
+
+
+// do while loop
