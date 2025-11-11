3108a49a299ced54aba901c536b9a39dd3d7f2dd
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-12 00:29:22 +0530

    Practice some more question in session 2.

--- Practice2/script.js
+++ Practice2/script.js
@@ -44,7 +44,7 @@ else {
 let chances = 3;
 let password = "shaurya"
 while (chances > 0) {
-    let tryPass = prompt(`Enter the password. You have ${chances} chancs left`);
+    let tryPass = prompt(`Enter the password. You have ${chances} chances left`);
     if (tryPass === password) {
         console.log("Correct Password! Access Granted");
         break;
@@ -82,4 +82,84 @@ console.log(`The word "Yes" was typed ${countYes} times.`);
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Question 13
-// 
+// Print numbers divisible by 7 from 1 to 50
+
+for (let k = 1; k < 50; k++){
+    if (k % 7 === 0) {
+        console.log(k);
+    }
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Question 14
+// Sum of all odd numbers from 1 to 30. Print final sum
+let sum = 0;
+for (let a = 1; a <= 30; a++){
+    if (a % 2 !== 0) {
+        sum += a;
+    }    
+}
+console.log(sum);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Question 15
+// Keep asking number until user enters an even number.
+
+while (true) {
+    let num = +prompt("Enter any number: ")
+    if (num % 2 === 0)
+        break;
+}
+console.log("Exited bcz entered an even number");
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Question 16
+// print all numbers between two numbers entered by the user.
+
+let start = +prompt("Enter the starting number:");
+let end = +prompt("Enter the ending number:");
+
+while (start != end) {
+    console.log(start);
+    start++;
+}
+
+
+// let start = +prompt("Enter the starting number:");
+// let end = +prompt("Enter the ending number:");
+
+// console.log("--- Numbers Between ---");
+
+// // Case 1: Counting Up (start < end)
+// if (start < end) {
+//     while (start <= end) { // Use <= to include the 'end' number
+//         console.log(start);
+//         start++;
+//     }
+// }
+// // Case 2: Counting Down (start > end)
+// else if (start > end) {
+//     while (start >= end) { // Use >= to include the 'end' number
+//         console.log(start);
+//         start--; // Must decrement when counting down
+//     }
+// }
+// // Case 3: Start and End are the same
+// else {
+//     console.log(start);
+// }
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Question 17
+// Print only first three odd numbers from 1 to 20. stop using break.
+
+let countOdd = 0;
+
+while (countOdd != 3) {
+    
+}
