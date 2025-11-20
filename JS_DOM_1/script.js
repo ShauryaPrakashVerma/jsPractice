8fdaec52582556e537702efbe310ccb3d6243f68
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-20 18:28:05 +0530

    Learn about DOM

---
+++ JS_DOM_1/script.js
@@ -0,0 +1,24 @@
+// Four pillars of DOM:-
+// 1. Selection of an elemnet
+// 2. Changing HTML
+// 3. Changing CSS
+// 4. Event Listener
+
+// SELECTION OF AN ELEMENT-
+// var a = document.querySelector("h1");
+// console.log(a);
+
+// Changing HTML
+// var a = document.querySelector("h1");
+// a.innerHTML = "Changed";
+
+// Changing CSS
+// var a = document.querySelector("h1");
+// a.style.color = "green";    // write in camel case
+// a.style.backgroundColor = "red";
+
+// EVENT LISTENER
+// var a = document.querySelector("h1");
+// a.addEventListener("click", function () {
+//     console.log("hello");
+// })
