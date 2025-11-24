b97709ccc929697aa0e3aab1f19235bf997d4bec
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-24 19:42:53 +0530

    Partially complete Instagram Like feature.

---
+++ JS_DOM_5/script.js
@@ -0,0 +1,9 @@
+var pic = document.querySelector("#pic");
+var heart = document.querySelector("#love");
+
+pic.addEventListener("click", function () {
+    
+    // heart.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+    heart.style.opacity = "1";
+    console.log("clicked");
+})
