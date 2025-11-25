88ea051fd11496e0fe87b50d128605250ee05dab
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-11-25 12:16:34 +0530

    Complete Insta Like Feature

--- JS_DOM_5/script.js
+++ JS_DOM_5/script.js
@@ -1,9 +1,14 @@
 var pic = document.querySelector("#pic");
 var heart = document.querySelector("#love");
 
-pic.addEventListener("click", function () {
+pic.addEventListener("dblclick", function () {
     
-    // heart.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+    heart.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
     heart.style.opacity = "1";
     console.log("clicked");
 })
+
+setTimeout(function () {
+    heart.style.opacity = "0";
+    // heart.style.transform = 'translate(-50%,-50%) scale(0) rotate(0deg)'
+}, 3000)
