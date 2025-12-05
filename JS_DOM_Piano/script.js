9196f421870637b913ab0731fb38ef3d276b1d51
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-12-05 23:42:33 +0530

    Create piano by click

---
+++ JS_DOM_Piano/script.js
@@ -0,0 +1,29 @@
+// Mouse Press Play
+
+// var q = document.querySelectorAll("#keys div");
+// const audFiles = ["key24.mp3","key24.mp3","key23.mp3","key22.mp3","key21.mp3","key20.mp3","key19.mp3","key18.mp3","key17.mp3","key16.mp3","key15.mp3","key14.mp3","key13.mp3","key12.mp3","key11.mp3","key10.mp3","key09.mp3","key08.mp3","key07.mp3","key06.mp3","key05.mp3","key04.mp3","key03.mp3","key02.mp3"]
+// console.log(q);
+// var num = 1;
+// q.forEach(function (user) {
+//     // console.log(num++);
+//     // console.log(user);
+//     user.addEventListener("click", function () {
+//         console.log(user.innerText);
+//         var audio = new Audio(`${audFiles[num++]}`);
+//         audio.play();
+//     })
+
+// })
+
+
+// Keyboard Key Press Play
+
+document.addEventListener("keypress", function (abc) {
+    console.log(abc.key);
+})
+
+const keys = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L"]
+
+q.forEach(function (user) {
+    
+})
