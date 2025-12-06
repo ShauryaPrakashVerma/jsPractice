0b331c78f023be5c2b3b64ed4a2454b431ba0f7e
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-12-06 19:30:28 +0530

    Add volume control functionality.

--- JS_DOM_Piano/script.js
+++ JS_DOM_Piano/script.js
@@ -1,24 +1,25 @@
 // Mouse Press Play
 
-// Keep your selector and your audio files list exactly the same
 var q = document.querySelectorAll("#keys div");
+var volumeControl = document.getElementById("volumeSlider");
 
-// Add this at the top with your audFiles
 const keyboardKeys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l"];
 const audFiles = ["key24.mp3","key24.mp3","key23.mp3","key22.mp3","key21.mp3","key20.mp3","key19.mp3","key18.mp3","key17.mp3","key16.mp3","key15.mp3","key14.mp3","key13.mp3","key12.mp3","key11.mp3","key10.mp3","key09.mp3","key08.mp3","key07.mp3","key06.mp3","key05.mp3","key04.mp3","key03.mp3","key02.mp3"]
 
-// DELETE "var num = 1;" -- You don't need this anymore!
-
-// Update the loop to accept the 'index'
 q.forEach(function (user, index) { 
     
     user.addEventListener("click", function () {
-        // purely for debugging to see which key number this is
+        user.style.backgroundColor = "grey"
+
+        setTimeout(() => {
+            user.style.backgroundColor = "white"
+        },200)
+
         console.log("This is key number: " + index); 
         
-        // Use 'index' to grab the specific file for THIS specific key
         var audio = new Audio(audFiles[index]); 
-        
+
+        audio.volume = volumeControl.value; // for controlling the volume
         audio.play();
     });
 
@@ -27,27 +28,18 @@ q.forEach(function (user, index) {
 });
 
 // Keyboard Key Press Play
-// This goes outside your forEach loop
 document.addEventListener("keydown", function(event) {
     
-    // 1. Get the letter pressed (convert to lowercase so 'Q' and 'q' both work)
     const keyPressed = event.key.toLowerCase();
 
-    // 2. Check: Is this letter in our list?
-    // .indexOf() tells us the position number (0, 1, 2...). 
-    // If it returns -1, it means the key is NOT in the list.
     const index = keyboardKeys.indexOf(keyPressed);
 
-    // 3. If we found a valid key (index is not -1)
     if (index !== -1) {
         
-        // Play the sound at that specific Index
         var audio = new Audio(audFiles[index]);
         audio.play();
         
-        // Optional: Trigger a visual click on the key so it lights up!
-        // This 'clicks' the HTML element for you
-        // q[index].click(); 
+        q[index].click(); 
     }
 });
 
