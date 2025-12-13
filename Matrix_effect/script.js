4173350c2d608f60dc1669abc6b637cecd6e6282
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-12-13 16:06:55 +0530

    Complete Matrix Effect.

--- Matrix_effect/script.js
+++ Matrix_effect/script.js
@@ -1,23 +1,45 @@
-var p = document.querySelector("p")
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+var p = document.querySelector("p");
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const text = p.innerText
 
+let iteration = 0;
 
-p.addEventListener("mouseenter", function () {
-    
-    setInterval(() => {
-        const str = p.innerText.split("").map((char, index) => {
-            return characters.split("")[Math.floor(Math.random() * 52)]
-            // putting * 53 slowly eats away the string . ie. the string slowly decreases the size.
-            // https://gemini.google.com/share/70d68748b604
-            // this is 53 is beyond the size of the characters string.
-            // can be used as a feature
-            // possibly to animate
-        }).join("")
+function randomText() {
+    const str = text.split("").map((char, index) => {
+        if (index < iteration) {
+            return char
+        }
+        return characters.split("")[Math.floor(Math.random()*52)]
+    }).join("")
 
-        
-        p.innerText = str
-    },50)
-    
+    p.innerText = str
 
+    iteration += 0.2;
+}
 
+p.addEventListener("mouseenter", function (char, index) {
+    setInterval(randomText, 30);    
 })
+
+
+// p.addEventListener("mouseenter", function (char,index) {
+//   setInterval(() => {
+//       const str = p.innerText.split("").map((char, index) => {
+//           if (index < iteration) {
+//               return char
+//           }
+//           return characters.split("")[Math.floor(Math.random() * 52)];
+//         // putting * 53 slowly eats away the string . ie. the string slowly decreases the size.
+//         // https://gemini.google.com/share/70d68748b604
+//         // this is 53 is beyond the size of the characters string.
+//         // can be used as a feature
+//         // possibly to animate
+//       })
+//       .join("");
+
+//     iteration += 2;
+
+//     console.log(iteration, char, index);
+//     p.innerText = str;
+//   }, 50);
+// });
